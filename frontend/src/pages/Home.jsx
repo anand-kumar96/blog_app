9e89aa19bed168b881c8b08a454668b3a7c354c5
Anand Kumar <hyeiamaka06@gmail.com>
2024-05-08 10:55:58 +0530

    updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,8 @@ import { useSearchParams } from "react-router-dom";
 const Home = () => {
   const [blogs,setBlogs] = useState(null)
   let [searchParams, setSearchParams] = useSearchParams();
-
+  const[isLoading,setIsLoading] = useState(true);
+  const [isError,setIsError] = useState(false);
     /// All Blogs Or Filter blogs
      useEffect(()=>{
         const fetchData = async () => {
@@ -13,18 +14,23 @@ const Home = () => {
               if(searchParams.get('category')) {
                 const {data} = await getBlogByCategory(searchParams.get('category'));
                 setBlogs(data)
+                setIsLoading(false);
               } else {
                 const {data} = await getBlogs();
                 setBlogs(data)
+                setIsLoading(false);
               }
             } catch (error) {
+              setIsError(true);
               console.error('Error fetching data:', error);
             }
+            setIsLoading(false);
         };
         fetchData();
      },[searchParams])
-
-
+  
+  if(isLoading) return <div><h4 className="flex justify-center">Loading please wait...</h4></div>
+  if(isError) return <div><h1 className="flex justify-center"></h1>Error while fetching data</div>
   return (
     <div>
       {(blogs==null || blogs.length == 0) ? <h1 className='flex justify-center '>No blog created Yet!!</h1> 
